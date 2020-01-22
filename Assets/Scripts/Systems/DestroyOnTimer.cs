4bf2c85c93bada9bbb3a61ad22fdc714488b337a
EngHell <migueln6@gmail.com>
2020-01-21 21:09:41 -0600

    cleared old code

--- Assets/Scripts/Systems/DestroyOnTimer.cs
+++ Assets/Scripts/Systems/DestroyOnTimer.cs
@@ -9,49 +9,8 @@ using Unity.Burst;
 
 namespace Systems
 {
-    [UpdateAfter(typeof(EndFramePhysicsSystem))]
     public class DestroyOnTimer : ComponentSystem
     {
-        EndSimulationEntityCommandBufferSystem endSimulationCBS;
-        public struct DestroyOnTimerJob : IJobForEachWithEntity<Components.LifeTime>
-        {
-            public EntityCommandBuffer cmd;
-            public float deltaTime;
-
-
-            public void Execute(Entity entity, int index, ref LifeTime lifeTime)
-            {
-                if (lifeTime.Value <= 0)
-
-                    cmd.DestroyEntity(entity);
-                else
-                {
-                    Debug.Log("Current: " + lifeTime.Value + " delta:" + deltaTime);
-                    lifeTime.Value -= deltaTime;
-                }
-
-            }
-        }
-
-        protected override void OnCreate()
-        {
-            base.OnCreate();
-            endSimulationCBS = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-        }
-
-        protected JobHandle OnUpdate(JobHandle inputDeps)
-        {
-            float deltaTime = Time.DeltaTime;
-            EntityCommandBuffer cmd = endSimulationCBS.PostUpdateCommands;
-
-            var job = new DestroyOnTimerJob { cmd = cmd, deltaTime = deltaTime };
-
-            var jobHandle = job.Schedule(this, inputDeps);
-
-            endSimulationCBS.AddJobHandleForProducer(jobHandle);
-
-            return jobHandle;
-        }
 
         [BurstCompile]
         protected override void OnUpdate()
